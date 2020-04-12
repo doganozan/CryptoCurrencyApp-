bb3d1436da8f1140c1aa6547f142c97ed8660bee
Ozan Doğan <45738864+doganozan@users.noreply.github.com>
2020-04-12 13:44:01 -0400

    fixing typo

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Crypto Currency',
+      title: 'Crypto Currency App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.pink,
