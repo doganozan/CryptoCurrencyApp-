d1910277e7d9f034b1297c3647e03fa399d07de3
Ozan Doğan <45738864+doganozan@users.noreply.github.com>
2020-04-12 12:11:10 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cripto Currency',
+      title: 'Crypto Currency',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.pink,
